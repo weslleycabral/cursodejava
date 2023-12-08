5f6ab4ab59410260ff1c2a68aab7936ce26d9197
Weslley Juan <weslley.deve@gmail.com>
2023-12-08 11:53:23 -0300

    Usando Git pela primeira vez

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.entity.*;
+
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var agora = LocalDate.parse("2022-02-02");
+        System.out.println(agora);
+
+    }
+}
